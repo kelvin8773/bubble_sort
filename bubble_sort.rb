385ebed345c1acb0e1be2646a8bff89d52a0e9cc
Kelvin Liang <kelvin9877@gmail.com>
2019-05-27 13:14:20 +0800

    remove example and debug code

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -2,7 +2,6 @@ def bubble_sort(arr)
   n = arr.length
 
   (n-1).times do
-    p arr
     sorted = true
 
     for i in 0..n-2
@@ -17,14 +16,3 @@ def bubble_sort(arr)
   return arr
 end
 
-array1 = [5, 4, 3, 2, 1]
-
-array2 = [4, 3, 78, 2, 0, 2]
-
-array3 = [4, 3, 78, 2, 0, 2, 7, 1, 5, 3, 32, 18]
-
-array4 = [4, 3, 78, 2, 0, 2, 5, 19, 2, 12, 67, 89]
-
-# change the array name in following line to see the result! 
-
-puts "Sorted Array is: #{bubble_sort(array3)}"
--- bubble_sort_by.rb
+++ bubble_sort_by.rb
@@ -15,27 +15,3 @@ def bubble_sort_by(arr)
   return arr
 end
 
-arr_string1 = ["hi","hello","hey"]
-
-arr_string2 = ["hi","hello","hey", "o", "Yes, Please!", "no", "I", "Solution"]
-
-arr_string3 = ["go", "come","hi","hello","hey", "what", "how"]
-
-arr_string4 = ["hi","yes, please", "left", "right", "ok", "hello","hey"]
-
-array1 = [5, 4, 3, 2, 1]
-
-array2 = [4, 3, 78, 2, 0, 2]
-
-array3 = [4, 3, 78, 2, 0, 2, 7, 1, 5, 3, 32, 18]
-
-array4 = [4, 3, 78, 2, 0, 2, 5, 19, 2, 12, 67, 89]
-
-# change the array name in following line to see the result! 
-
-p bubble_sort_by(arr_string2) {|left,right| left.length - right.length } #by length
-
-p bubble_sort_by(arr_string3) {|left,right| left[0] <=> right[0] } #by alphabetic 
-
-p bubble_sort_by(array2) {|left,right| left - right } # by value 
-
